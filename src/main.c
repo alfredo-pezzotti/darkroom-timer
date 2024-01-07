011b76ac4cbb8e9e1c8f1b0496d6e5a46e60bf3e
Alfredo pezzotti <alfredo.pezzotti@yahoo.com>
2024-01-07 18:05:31 +0100

    adds definitions for PORTC shift register communication

--- src/main.c
+++ src/main.c
@@ -1,19 +1,29 @@
+
+
+#include "main.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
 #define LED_PIN (2)
 
 #ifdef MOCK
-int mock_portb;
-# define LED_PORT mock_portb
+    int     mock_portb;
+    uint8_t mock_portc;
+    # define LED_PORT mock_portb
+    #define SHR_PORT  mock_portc
 #else
-# define LED_PORT PORTB
+    # define LED_PORT PORTB
+    #define SHR_PORT PORTC
 #endif
 
-int main(void)
+void main(void)
 {
+    // Pinout setup:
     DDRB |= 1 << LED_PIN;
-    while (1) {
+    //DDRC |=
+
+    while (EXECUTE_FOREVER)
+    {
         LED_PORT ^= 1 << LED_PIN;
         _delay_ms(1000);
     }
---
+++ src/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// PIN offsets:
+#define PC_SHR_CLR              0x01u
+#define PC_SHR_CLK              0x02u
+#define PC_SHR_STRCLK           0x04u
+#define PC_SHR_OUTENABLE        0x08u
+#define PC_SHR_DATA             0x10u
+
+#define EXECUTE_FOREVER         0xA5
+
+#endif
