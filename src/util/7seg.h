16df6179b13eca9bac804905f5bfb7e273601d9f
Alfredo pezzotti <alfredo.pezzotti@yahoo.com>
2024-01-14 11:28:21 +0100

    adds inverted digit defines for rotated displays

--- src/util/7seg.h
+++ src/util/7seg.h
@@ -4,14 +4,24 @@
 
 // digits:
 #define 7SEG_0              0b11111100
+#define 7SEG_0_i            0b11111100
 #define 7SEG_1              0b01100000
+#define 7SEG_1_i            0b00001100
 #define 7SEG_2              0b11011010
+#define 7SEG_2_i            7SEG_2
 #define 7SEG_3              0b11110010
+#define 7SEG_3_i            0b10011110
 #define 7SEG_4              0b01100110
+#define 7SEG_4_i            0b00101110
 #define 7SEG_5              0b10110110
+#define 7SEG_5_i            7SEG_5
 #define 7SEG_6              0b10111110
+#define 7SEG_6_i            0b11110110
 #define 7SEG_7              0b11100000
+#define 7SEG_7_i            0b00011100
 #define 7SEG_8              0b11111110
+#define 7SEG_8_i            7SEG_8
 #define 7SEG_9              0b11110110
+#define 7SEG_9_i            0b10111110
 
 #endif
