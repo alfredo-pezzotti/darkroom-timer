10b1380ae5d19897144462b6d5560e194824ed80
Alfredo pezzotti <alfredo.pezzotti@yahoo.com>
2024-01-14 10:08:45 +0100

    adds 7 segment digits defines

--- src/util/7seg.h
+++ src/util/7seg.h
@@ -1,4 +1,17 @@
 #ifndef 7SEG_H
 #define 7SEG_H
 
+
+// digits:
+#define 7SEG_0              0b11111100
+#define 7SEG_1              0b01100000
+#define 7SEG_2              0b11011010
+#define 7SEG_3              0b11110010
+#define 7SEG_4              0b01100110
+#define 7SEG_5              0b10110110
+#define 7SEG_6              0b10111110
+#define 7SEG_7              0b11100000
+#define 7SEG_8              0b11111110
+#define 7SEG_9              0b11110110
+
 #endif
